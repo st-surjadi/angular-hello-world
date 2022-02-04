7e58db81d82d12af8122779b7c1a1133361ec872
Steven Surjadi <st.surjadi@gmail.com>
2022-02-04 14:09:00 +0700

    Pasang Sidenav di Dashboard.

---
+++ src/app/view/pages/dashboard/dashboard.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardRoutingModule } from './dashboard-routing.module';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+import { FormsModule } from '@angular/forms';
+
+
+const Material = [
+  MatSidenavModule,
+  MatCheckboxModule,
+  MatIconModule,
+  MatButtonModule
+]
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    Material,
+    FormsModule
+  ]
+})
+export class DashboardModule { }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,10 @@ module.exports = {
     content: [
       "./src/**/*.{html,ts}",
     ],
+    darkMode: 'class',
     theme: {
       extend: {},
     },
     plugins: [],
+    
 }
